9f5769b8855c85bbc7258d45deb668d4e24b9d17
Dan Rounds <dan.rounds@gmail.com>
2016-12-31 17:40:14 -0500

    test for GET request works

--- test/API-test.js
+++ test/API-test.js
@@ -10,3 +10,38 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 
+describe('Blog API', function() {
+
+    // activate server
+    before(function() {
+        return runServer(); 
+    });
+
+    // close server
+    after(function() {
+        return closeServer(); 
+    });
+
+    const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+
+    // GET request test -- strategy:
+    //  1. make GET request
+    //  2. inspect response to see if it has the right status and keys
+    it('should list blog posts on GET', function() {
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function(res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+
+                res.body.length.should.be.at.least(1);
+
+                res.body.forEach(function(item) {
+                    item.should.be.a('object');
+                    item.should.include.keys(expectedKeys);
+                });
+            });
+    });
+
+})
